8f64521e4b0a8e6564dbdfb0a030060f497c2c3b
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-07-27 13:36:21 +0000

    [Start]lesson2

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show' , as:  'show_book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
-  get '/top' => 'homes#top'
+  delete 'books/:id' => 'books#destroy'
+  get '/top' => 'homes#top', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
