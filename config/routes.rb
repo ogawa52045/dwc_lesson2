ea788a8362061c9ad54e69de099b512ae2b6fe63
ogawa52045 <138991739+ogawa52045@users.noreply.github.com>
2023-07-27 15:04:13 +0000

    [Finish]lesson2-until_page_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new' => 'books#new'
-  post 'books' => 'books#create'
-  get '/books' => 'books#index'
-  get 'books/:id' => 'books#show' , as:  'show_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy'
-  get '/top' => 'homes#top', as: 'destroy_book'
+  get '/top' => 'homes#top'
+  resources :books
+  root to:  'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
